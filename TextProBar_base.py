2e9b61ebf57ffc179eeeedc32de0f9b38471d678
Jokertion <645434934@qq.com>
2018-04-23 10:35:34 +0800

    简单文本进度条

---
+++ TextProBar_base.py
@@ -0,0 +1,12 @@
+#TextProBarV1.py
+#简单文本进度条
+import time 
+scale = 10
+print ("-------执行开始--------")
+for i in range(scale+1):
+	a = '*' * i
+	b = '.' * (scale - i)
+	c = (i/scale)*100
+	print ("{:^3.0f}%[{}->{}]".format(c,a,b))
+	time.sleep(0.1)
+print ("-------执行结束-------")
