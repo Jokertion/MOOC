4ca04626dc2bec17cd8e9c5c127fb36b8dee4f0c
Jokertion <645434934@qq.com>
2018-05-02 20:11:18 +0800

    基本统计值计算

---
+++ CalStatisticV1.py
@@ -0,0 +1,35 @@
+#CalStatisticV1.py
+#基本统计值计算
+#获得技能包1：从控制台获取多个不确定数据的方法
+def getNum(): 
+	nums = []
+	iNumStr = input("请输入数字（回车退出）: ")
+	while iNumStr != "":
+		nums.append(eval(iNumStr))
+		iNumStr =  input("请输入数字（回车退出）: ")
+	return nums
+
+def mean(numbers):		#计算平均数
+	s = 0.0 
+	for num in numbers:
+		s = s + num
+	return s / len(numbers) #len(numbers)：一组数的总个数
+	
+def dev(numbers, mean):	#计算方差
+	sdev = 0.0
+	for num in numbers :
+		sdev = sdev + (num - mean)**2
+	return pow(sdev / (len(numbers)-1), 0.5)
+
+def median(numbers): 	#计算中位数
+	sorted(numbers)    #sorted(numbers)： 对列表进行排序
+	size = len(numbers)
+	if size % 2 == 0:
+		med = (numbers[size//2-1] + numbers[size//2])/2
+	else:
+		med = numbers[size//2]
+	return med
+	
+n = getNum()
+m=mean(n)
+print ("平均值:{},方差:{:.2},中位数:{}.".format(m, dev(n,m), median(n)))
